82ce94650a79d10732cf2a1c35db90c1606bd4d5
Michael Larkin <larkin.michael@verizon.net>
2019-07-10 21:12:14 -0400

    Checkboxes used by FormContainer (a.k.a. Filter) are working. Need to wire it up to send preferences to server

--- src/components/Filter/FormContainer.js
+++ src/components/Filter/FormContainer.js
@@ -17,13 +17,30 @@ class FormContainer extends Component {
 
     handleCheckBox = (e) => {
 
-        console.log("handleCheckBox - e: ", e);
-        console.log("handleCheckBox - e.target.id: ", e.target.id);
-        console.log("handleCheckBox - typeof e.target.id: ", typeof e.target.id);
-
         const newSelection = e.target.value;
 
-        let oldSelectionArray = this.state.dataCenters;
+        let oldSelectionArray = [];
+
+        switch(e.target.id) {
+            case 'dataCenters':
+                oldSelectionArray = this.state.dataCenters;
+                break;
+            case 'zones':
+                oldSelectionArray = this.state.zones;
+                break;
+            case 'environments':
+                oldSelectionArray = this.state.environments;
+                break;
+            case 'operatingSystemTypes':
+                oldSelectionArray = this.state.operatingSystemTypes;
+                break;
+            case 'operatingSystems':
+                oldSelectionArray = this.state.operatingSystems;
+                break;
+            default:
+                return null;
+        }
+
         let newSelectionArray = [];
 
         if(oldSelectionArray.indexOf(newSelection) > -1) {
@@ -31,8 +48,6 @@ class FormContainer extends Component {
         } else {
             newSelectionArray = [...oldSelectionArray, newSelection];
         }
-
-        console.log("handleCheckbox - newSelectionArray: ", newSelectionArray);
 
         this.setState({[e.target.id]: newSelectionArray});
 
@@ -48,10 +63,33 @@ class FormContainer extends Component {
                           selectedOptions={this.state.dataCenters}
                           handleChange={this.handleCheckBox}
                 />
+                <Checkbox title='Zones'
+                          name='zones'
+                          options={ ['Corporate', 'DMZ', 'PCI', 'PII'] }
+                          selectedOptions={this.state.zones}
+                          handleChange={this.handleCheckBox}
+                />
+                <Checkbox title='Environments'
+                          name='environments'
+                          options={ ['Production', 'UAT', 'QA', 'Test', 'Development'] }
+                          selectedOptions={this.state.environments}
+                          handleChange={this.handleCheckBox}
+                />
+                <Checkbox title='Operating System Types'
+                                        name='operatingSystemTypes'
+                                        options={ ['Physical', 'Virtual'] }
+                                        selectedOptions={this.state.operatingSystemTypes}
+                                        handleChange={this.handleCheckBox}
+            />
+                <Checkbox title='Operating Systems'
+                          name='operatingSystems'
+                          options={ ['Linux', 'Unix', 'Windows'] }
+                          selectedOptions={this.state.operatingSystems}
+                          handleChange={this.handleCheckBox}
+                />
             </form>
         );
     };
-
 }
 
 export default FormContainer;
